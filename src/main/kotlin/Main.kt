85c63d2cecab8248ea2f59289f70684e89adcebc
ryota <kurokawa.ryota.1124@gmail.com>
2024-07-07 13:59:26 +0900

    coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    GlobalScope.launch {
-        delay(1000L)
-        println("Hello World")
+    runBlocking {
+        launch {
+            delay(1000L)
+            println("hello")
+        }
+        println("Kotlin")
     }
-    println("My kotlin")
-    Thread.sleep(2000L)
 }
